a3cef98599c375a3254f6e3e2d6d64ff7768f18c
OneNeptune <5749451+OneNeptune@users.noreply.github.com>
2024-05-30 20:52:52 -0500

    add thor, optimize

--- lib/spongebobify.rb
+++ lib/spongebobify.rb
@@ -1,15 +1,28 @@
 # frozen_string_literal: true
 
 require 'spongebobify/version'
+require 'spongebobify/cli'
+
+def is_lowercase(byte)
+  byte >= 97 && byte <= 122
+end
+
+def is_uppercase(byte)
+  byte >= 65 && byte <= 90
+end
 
 module Spongebobify
-  def self.process(string)
-    new_string = string.downcase
+  def self.process(input_string)
+    index = 0
+    new_string = String.new
 
-    (1...new_string.length).step(2).each do |index|
-      next unless new_string[index].ord.between?(97, 122)
-
-      new_string[index] = (new_string[index].ord ^ 32).chr
+    input_string.each_byte do |byte|
+      if index % 2 == 0
+        new_string << (is_lowercase(byte) ? (byte ^ 32).chr : byte.chr)
+      else
+        new_string << (is_uppercase(byte) ? (byte ^ 32).chr : byte.chr)
+      end
+      index += 1
     end
 
     new_string
---
+++ lib/spongebobify/cli.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'thor'
+require 'spongebobify'
+
+module Spongebobify
+  class CLI < Thor
+    desc "spongebobify TEXT", "Converts TEXT to spongebob case"
+    def spongebobify(text = nil)
+      if text.nil? && $stdin.tty?
+        puts "No input provided. Use 'spongebobify \"some text\"' or 'spongebobify << ./some_file.txt'"
+        exit 1
+      end
+      
+      input = text || $stdin.read
+      puts Spongebobify.process(input)
+    end
+  end
+end
